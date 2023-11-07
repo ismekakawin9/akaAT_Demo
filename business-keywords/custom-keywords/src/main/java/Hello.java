6dfe43755c9948ca4866a5868acb2c802298c9b6
Dat <ismekakawin9@gmail.com>
2023-11-07 15:32:22 +0700

    update

--- business-keywords/custom-keywords/src/main/java/Hello.java
+++ business-keywords/custom-keywords/src/main/java/Hello.java
@@ -1,6 +1,28 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.fpt.ivs.at.core.object.UIObject;
+import com.fpt.ivs.at.core.utilities.KeywordAssertion;
+import com.fpt.ivs.at.core.utilities.WebDriverUtilities;
 
 public class Hello {
+    RemoteWebDriver driver = WebDriverUtilities.getDriver();
     public String test() {
         return "Hello World!!";
+    }
+
+    public void sendKey(UIObject target, String key){
+        WebElement element = target.convertToWebElement(driver);
+        element.sendKeys(key);
+    }
+
+    public void verifyElementVisible(UIObject target){
+        WebElement element = target.convertToWebElement(driver);
+        KeywordAssertion.assertConditionFulfilled(element.isDisplayed());
+    }
+
+    public String getTextFromElement(UIObject target){
+        WebElement element = target.convertToWebElement(driver);
+        return element.getText();
     }
 }
