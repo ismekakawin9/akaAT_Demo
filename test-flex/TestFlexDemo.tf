0f7cf95ae1473944195f9884e7266aa88ac59536
Dat <ismekakawin9@gmail.com>
2023-11-13 15:23:55 +0700

    update

---
+++ test-flex/TestFlexDemo.tf
@@ -0,0 +1,58 @@
+{
+  "varNode": {
+    "descendants": {
+      "TEST": {
+        "descendants": {}
+      }
+    },
+    "value": "b2738bf8-739c-4c58-aa4b-fb400ffe67b2"
+  },
+  "platform": "Web",
+  "executionList": [
+    {
+      "tcId": "cc4c0d69-f7cc-447b-afdd-92cfac1fa78a",
+      "profileId": "",
+      "platform": "Web",
+      "uuid": "fe8f8002-6bb3-4f66-ab4c-5f539d45e30f",
+      "type": "test_case",
+      "name": "SignUp",
+      "params": [
+        {
+          "name": "username",
+          "type": "String",
+          "defaultValue": "",
+          "source": ""
+        },
+        {
+          "name": "password",
+          "type": "String",
+          "defaultValue": "",
+          "source": ""
+        }
+      ]
+    },
+    {
+      "tcId": "7d2dca1a-8cbe-450c-93a0-260e1842426b",
+      "profileId": "",
+      "platform": "Web",
+      "uuid": "f43e6d62-3860-4f0e-91fe-02e72cccd96e",
+      "type": "test_case",
+      "name": "Login",
+      "params": [
+        {
+          "name": "password",
+          "type": "String",
+          "defaultValue": "ThanhDat1!",
+          "source": ""
+        },
+        {
+          "name": "username",
+          "type": "String",
+          "defaultValue": "datnt",
+          "source": ""
+        }
+      ]
+    }
+  ],
+  "uuid": "b2738bf8-739c-4c58-aa4b-fb400ffe67b2"
+}
